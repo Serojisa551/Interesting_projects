2a3a2f0972777942170f2c02743967f6d8b38b94
Serojisa551 <116907048+Serojisa551@users.noreply.github.com>
2023-04-02 02:36:17 +0400

    Added 2 commands 'start' and 'deleteall' the button click handler function is also added

--- tg_bot/tg_db.py
+++ tg_bot/tg_db.py
@@ -1,4 +1,4 @@
-from telebot.async_telebot import AsyncTeleBot
+from telebot.async_telebot import AsyncTeleBot, types
 import asyncio
 import psycopg2
 
@@ -10,9 +10,23 @@ cursor = conn.cursor()
 
 
 # TODO(
-# Handle '/allpfilms'
-@bot.message_handler(commands=["allfilms"])
+# Handle '/allpfilms', '/start' and '/deleteall'
+@bot.message_handler(commands=["allfilms", "start", "deleteall"])
 async def send_welcome(message):
+    if message.text == "/deleteall":
+        markup = types.InlineKeyboardMarkup(row_width=2)
+        yes = types.InlineKeyboardButton("yes", callback_data="del_yes")
+        no = types.InlineKeyboardButton("no", callback_data="del_no")
+        markup.add(yes, no)
+        await bot.send_message(
+            message.chat.id,
+            text="Are you sure you want to delete everything",
+            reply_markup=markup,
+        )
+    if message.text == "/start":
+        await bot.send_message(
+            message.chat.id, text="The bot is activated and ready to work"
+        )
     if message.text == "/allfilms":
         cursor.execute("SELECT name FROM kino")
         result = cursor.fetchall()
@@ -25,6 +39,25 @@ async def send_welcome(message):
             await bot.send_message(message.chat.id, text=tpl)
             if tpl == result[-1]:
                 break
+
+
+# Here the button click is being processed
+@bot.callback_query_handler(func=lambda call: True)
+async def callback(call):
+    if call.message:
+        if call.data == "del_yes":
+            cursor.execute("TRUNCATE TABLE kino")
+            await bot.edit_message_text(
+                chat_id=call.message.chat.id,
+                message_id=call.message.id,
+                text="Аll deleted successfully",
+            )
+        if call.data == "del_no":
+            await bot.edit_message_text(
+                chat_id=call.message.chat.id,
+                message_id=call.message.id,
+                text="Okay I left the process",
+            )
 
 
 # The reported user is saved here
